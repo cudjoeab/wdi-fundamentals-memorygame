5f37f03a3fa50111b45e890a94df961b184bf7f6
Abigail Cudjoe <ABI@Duncans-MacBook-Air.local>
2019-06-28 01:30:34 -0400

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,10 @@
+console.log("up and running!"); 
+
+var cardOne = "queen";
+var cardTwo = "king";
+var cardThree = "queen";
+var cardFour = "king"; 
+
+console.log("User Flipped" + " " + cardOne); 
+console.log ("User Flipped" + " " + cardTWo); 
+
