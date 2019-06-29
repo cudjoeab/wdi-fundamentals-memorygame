557e7580e9c206541a5adad345a3b2373ff80ffe
Abigail Cudjoe <ABI@Duncans-MacBook-Air.local>
2019-06-29 18:19:23 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,35 +1,27 @@
-console.log("up and running!"); 
-
-//old variables from past unit
-/*
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";*/
-
-console.log("User Flipped" + " " + cardOne); 
-console.log ("User Flipped" + " " + cardTwo); 
+console.log("Up and running!"); 
 
 var cards = [ "queen", "queen", "king", "king"];
 var cardsInPlay = []; 
-cardsInPlay.length; 
-
-var cardOne = "queen";
-var cardTwo = "queen";
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
 
-console.log("User Flipped Queen"); 
-console.log("User Flipped King"); 
-console.log(cardsInPlay.length); 
 
-/*if(cardsInPlay.length === 2) {
-  console.log("checking"); 
-} */ 
+function flipCard(cardId) {
+	console.log("User flipped" + " " + cards[cardId]); 
+  cardsInPlay.push(cards[cardId]);
 
-if (cardsInPlay[0] === cardsInPlay[1]) {
+  checkForMatch(); 
+  
+  };
+
+function checkForMatch() { 
+  if (cardsInPlay[cardId] === cardsInPlay[cardId]) {
   alert("You found a match!");
-} else if (cardsInPlay[2] === cardsInPlay[3]) {
-  alert("Sorry, try again.");
-}
+	} else {
+		console.log("Sorry, try again.")
+	}; 
+
+};
+
+flipCard(0);
+flipCard(1); 
+
