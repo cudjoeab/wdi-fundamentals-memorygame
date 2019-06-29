f8c686d60bddf42c807623358e87296301695a8c
Abigail Cudjoe <ABI@Duncans-MacBook-Air.local>
2019-06-29 19:52:49 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,13 +1,34 @@
 console.log("Up and running!"); 
 
-var cards = [ "queen", "queen", "king", "king"];
+var cardId = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png",
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/kings-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+];
 var cardsInPlay = []; 
 
 
 
 function flipCard(cardId) {
-	console.log("User flipped" + " " + cards[cardId]); 
-  cardsInPlay.push(cards[cardId]);
+	console.log("User flipped  " + cardsInPlay[cardId].rank); 
+  cardsInPlay.push(cardsInPlay[cardId]);
 
   checkForMatch(); 
   
@@ -22,6 +43,6 @@ function checkForMatch() {
 
 };
 
-flipCard(0);
-flipCard(1); 
+flipCard(1);
+flipCard(2); 
 
