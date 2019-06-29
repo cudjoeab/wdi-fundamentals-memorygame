9a0978803f25fb7a964b506371e18ad23b0d9280
Abigail Cudjoe <ABI@Duncans-MacBook-Air.local>
2019-06-29 11:49:14 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,10 +1,35 @@
 console.log("up and running!"); 
 
+//old variables from past unit
+/*
 var cardOne = "queen";
 var cardTwo = "king";
 var cardThree = "queen";
-var cardFour = "king"; 
+var cardFour = "king";*/
 
 console.log("User Flipped" + " " + cardOne); 
-console.log ("User Flipped" + " " + cardTWo); 
+console.log ("User Flipped" + " " + cardTwo); 
 
+var cards = [ "queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+cardsInPlay.length; 
+
+var cardOne = "queen";
+var cardTwo = "queen";
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+
+console.log("User Flipped Queen"); 
+console.log("User Flipped King"); 
+console.log(cardsInPlay.length); 
+
+/*if(cardsInPlay.length === 2) {
+  console.log("checking"); 
+} */ 
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert("You found a match!");
+} else if (cardsInPlay[2] === cardsInPlay[3]) {
+  alert("Sorry, try again.");
+}
