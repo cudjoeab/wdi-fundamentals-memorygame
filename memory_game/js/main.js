66734917bff0368992f6ee3cc8c46da3d1731171
Abigail Cudjoe <ABI@Duncans-MacBook-Air.local>
2019-06-30 10:12:56 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,5 +1,30 @@
-console.log("Up and running!"); 
 
+
+
+
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = []; 
+
+var cardOne = cards[0]; 
+cardsInPlay.push(cardOne); 
+console.log("User flipped " + cardOne); 
+
+var cardTwo = cards[1];
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+var cardThree = cards[2];
+cardsInPlay.push(cardThree);
+console.log ("User flipped " + cardThree); 
+
+if (cards[0] === cards [1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.")
+};
+
+
+/*
 var cardId = [
 {
 	rank: "queen",
@@ -28,7 +53,7 @@ var cardsInPlay = [];
 
 function flipCard(cardId) {
 	console.log("User flipped  " + cardsInPlay[cardId].rank); 
-  cardsInPlay.push(cardsInPlay[cardId]);
+  	cardsInPlay.push(cardsInPlay[cardId]);
 
   checkForMatch(); 
   
@@ -43,6 +68,25 @@ function checkForMatch() {
 
 };
 
-flipCard(1);
-flipCard(2); 
+flipCard("queen");
+flipCard("king");
+
+//change element on click 
+/*
+document.querySelector('.circle').addEventListener('click', turnRed)
+
+var turnRed = function () {
+    this.style.backgroundColor = "red"; */ 
+
+    //Select all elements with the class .circle on the page
+var circles = document.querySelectorAll('.circle');
+
+//loop through each .circle element and add an event handler.
+for (var i = 0; i < circles.length; i++) {
+    circles[i].addEventListener('click', turnRed);
+}
+
+var turnRed = function () {
+    this.style.backgroundColor = "red";
+};
 
